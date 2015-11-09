3bc6a598a529c409335f1774bbb807cfc4f1a0b7
Michael Lipka <mvlipka@gmail.com>
2015-11-09 13:30:22 -0600

    Removed git reverting

--- main.py
+++ main.py
@@ -4,29 +4,21 @@ import git
 import time
 from git import Repo
 
-def check_git_status(git, branch, starting_branch):
-	result = git.checkout(branch)
+def watch_git(git, repo_path, to_call):
+	result = git.checkout("master")
 	if result == "Your branch is up-to-date with 'origin/master'.":
 		time.sleep(60)
-		check_git_status(git, branch, starting_branch)
+		watch_git(git, repo_path, to_call)
 	else:
 		git.pull()
-		revert_git_state(git, starting_branch)
-
-def revert_git_state(git, branch):
-	git.checkout(branch)
-	git.pull()
-	print "Reverted back to '{}'".format(branch)
+		time.sleep(60)
+		os.system("python {} {}".format(os.path.abspath(to_call), os.path.abspath(repo_path)))
+		watch_git(git, repo_path, to_call)
 
 if __name__ == '__main__':
 	repo_path = str(sys.argv[1])
-	repo_branch = str(sys.argv[2])
+	to_call = str(sys.argv[2])
 	repo = Repo(os.path.abspath(repo_path))
 	git = repo.git
-	starting_branch = repo.active_branch
 
-	import atexit
-	atexit.register(revert_git_state, git, starting_branch.name)
-
-	check_git_status(git, repo_branch, starting_branch.name)
-	
+	watch_git(git, repo_path, to_call)
